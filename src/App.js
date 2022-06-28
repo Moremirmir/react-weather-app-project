972ede22521b3a20d060acb9303d36110a399cde
Miriam <97915008+Moremirmir@users.noreply.github.com>
2022-06-28 14:20:39 -0500

    arranging files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <footer className="App-footer">
-        <h1>Weather App Project </h1>
-        <p>
-          Coded by{" "}
-          <a href="#" target="_blank">
-            Miriam DaSilva
-          </a>
-          . This is an{" "}
-          <a href="#" target="_blank">
-            open sourced
-          </a>
-          code
-        </p>
-      </footer>
+      <h1>Weather App Project </h1>
+      <p>
+        Coded by Miriam DaSilva. This is an{" "}
+        <a
+          href="https://github.com/Moremirmir/react-weather-app-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open sourced{" "}
+        </a>
+        code
+      </p>
     </div>
   );
 }
