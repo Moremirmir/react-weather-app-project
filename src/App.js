d8adef2b6dd2fd864abcf805125bf07cd51dc610
Miriam <97915008+Moremirmir@users.noreply.github.com>
2022-07-03 02:33:23 -0500

    Added API integration

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import "./App.css";
-
+import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App Project </h1>
-      <p>
-        Coded by Miriam DaSilva. This is an{" "}
-        <a
-          href="https://github.com/Moremirmir/react-weather-app-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open sourced{" "}
-        </a>
-        code
-      </p>
+      <div className="container">
+        <Weather defaultCity="New York" />
+        <p>
+          Coded by Miriam DaSilva. This is an{" "}
+          <a
+            href="https://github.com/Moremirmir/react-weather-app-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced{" "}
+          </a>
+          code on GitHub
+        </p>
+      </div>
     </div>
   );
 }
